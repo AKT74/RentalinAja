e3fe4e3ce109f7b0d888543682a1d209480ff5c0
AKT74 <nayakat74@gmail.com>
2022-12-22 11:17:20 +0700

    Tampilan Utama

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<div class="gambar1">
+    <img src="Asset 1.png" alt="">
+</div>
+<div class="menu">
+<a href=""><h1>Matic</h1></a>
+<a href=""><h1>Kopling</h1></a>
+<a href=""><h1>Manual</h1></a>
+</div>
+</body>
+</html>
